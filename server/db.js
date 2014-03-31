f3fbbf6c9b697532b2e18482c341973b52dedd3a
David Durst <davidbdurst@gmail.com>
2014-03-30 20:56:48 -0400

    removed dead code

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 Meteor.startup(function () {
     var fs = Npm.require('fs');
-    var stopStore = new FS.Store.GridFS("stops");
+    var StopImages = new FS.Store.GridFS("stopImages");
     var StopList = new Meteor.Collection("stopList"); 
     StopList.remove({})
 
@@ -8,28 +8,17 @@ Meteor.startup(function () {
     //Each image must be in a folder. It must have an img.json and img.JPG file.
     //The folder must be prepended with the substring 'pub-'
     var dirs = fs.readdirSync(pub).filter(function (file) {
-        /*console.log(pub + file);
-          if (file == '.pulse')
-          return false;
-          else
-          return false;*/
         return file.indexOf('pub-') != -1 && fs.statSync(pub + file).isDirectory();
     });
 
     for(var i = 0; i < dirs.length; i++) {
-        //console.log(pub + dirs[i] + "/img.json");
-        //console.log(fs.readFileSync(pub + dirs[i] + "/img.json", 'ascii'));
         imgJSON = 
             JSON.parse((fs.readFileSync(pub + dirs[i] + "/img.json", 'ascii')));
-        //console.log(StopList.findOne());
         if (StopList.find({id:1}).count() == 0) {
-            console.log(imgJSON.name);
-            //console.log(imgJSON[name]);
-            var tmp = new FS.Collection(imgJSON.name, { stores: [stopStore]});
+            var tmp = new FS.Collection(imgJSON.name, {stores: [StopImages]});
             tmp.insert(pub + dirs[i] + "/img.JPG");
             StopList.insert(imgJSON);
         }
     } 
-    console.log(StopList.find().count());
     
 });
