83223117d888a7d3b55b6ca7ee01d6462c66bc4d
David Durst <davidbdurst@gmail.com>
2014-04-01 00:47:04 -0400

    files in server, can't seem to get client to see them

--- client/views/lib/getData.js
+++ client/views/lib/getData.js
@@ -1,4 +1,14 @@
-StopList = new Meteor.Collection("stopList"); 
-
-Meteor.subscribe("stopList");
-
+AD = 1;
+Template.tour.getData = function () {
+    StopList = new Meteor.Collection("stopList"); 
+    ImageList = {};
+    Meteor.subscribe("stopList", function () {
+        StopList.find({}).forEach(function (stop) {
+            ImageList[stop.name] = new Meteor.Collection(stop.name);
+            //look at http://docs.mongodb.org/manual/reference/operator/query/in/
+            //for smart subscriptions
+            Meteor.subscribe(stop.name);
+        });
+    });
+    return {"StopList": StopList, "ImageList": ImageList};
+}
--- server/db.js
+++ server/db.js
@@ -2,8 +2,7 @@ var fs = Npm.require('fs');
 var StopImages = new FS.Store.GridFS("stopImages");
 var StopList = new Meteor.Collection("stopList"); 
 Meteor.publish("stopList", function () { return StopList.find({}) }); 
-//StopImages.remove({});
-//StopList.remove({});
+StopList.remove({});
 
 var pub = '../client/app/';
 //Each image must be in a folder. It must have an img.json and img.JPG file.
@@ -15,21 +14,25 @@ var dirs = fs.readdirSync(pub).filter(function (file) {
 for(var i = 0; i < dirs.length; i++) {
     imgJSON = 
         JSON.parse((fs.readFileSync(pub + dirs[i] + "/img.json", 'ascii')));
+    imgJSON.image = fs.readFileSync(pub + dirs[i] + "/img.JPG");
     if (StopList.find({id:1}).count() == 0) {
         //create a new collection for each image, allowing for fine grained
         //caching
         var tmp = new FS.Collection(imgJSON.name, {stores: [StopImages]});
+        tmp.remove({});
         tmp.insert(pub + dirs[i] + "/img.JPG");
         StopList.insert(imgJSON);
         Meteor.publish(imgJSON.name, function () { return tmp.find({}) });
+        console.log(imgJSON.name);
+        console.log(tmp.find({}).count());
         //NOTE: MUST CHECK THAT THESE WORK
-        images.allow({
-            download: function(userId, fileObj) { return true; },
-        });
-        images.deny({
+        tmp.allow({
+            download: function(userId, fileObj) { return false; },
             insert: function(userId, doc) { return true; },
             update: function(userId, doc, fields, modifier) { return true; },
             remove: function(userId, doc) { return true; },
+        });
+        tmp.deny({
         });
     }
 }     
