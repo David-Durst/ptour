36da6013111f1056953168fea862bbb4bbf8bf27
David Durst <davidbdurst@gmail.com>
2014-04-05 13:22:59 -0400

    removed dead code from db.js

--- server/db.js
+++ server/db.js
@@ -1,38 +1,9 @@
 var fs = Npm.require('fs');
-//var StopImagesStore = new FS.Store.GridFS("stopImagesStore");
-//var StopImages = new FS.Collection("stopImages", {stores: [StopImagesStore]});
 var StopList = new Meteor.Collection("stopList"); 
 
 Meteor.publish("stopList", function () { return StopList.find({}) }); 
-/*Meteor.publish("stopImages", function (locs) { 
-    if (StopImages.find({}).count() == 0) {
-        StopList.find({}).forEach(function (d) {
-            //StopImages.insert(pub + dirs[i] + "/img.JPG");
-            //StopImages.insert(pub + dirs[i] + "/img.JPG", "string");
-            //console.log(Meteor.absoluteUrl() + dirs[i] + "/img.JPG", "string");
-            StopImages.insert(d.imgUrl);
-            console.log(d.imgUrl);
-            //StopImages.insert("http://www.ptour.co/testTexture.jpg");
-            console.log(StopImages.findOne());
-            console.log(StopImages.find({}).count());
-        });
-    }
-    //return StopImages.find({$in: locs}) 
-    return StopImages.find({}) 
-});*/
 
 StopList.remove({});
-//StopImages.remove({});
-
-//NOTE: MUST CHECK THAT THESE WORK
-/*StopImages.allow({
-    download: function(userId, fileObj) { return false; },
-});
-StopImages.deny({
-    insert: function(userId, doc) { return true; },
-    update: function(userId, doc, fields, modifier) { return true; },
-    remove: function(userId, doc) { return true; },
-});*/
 
 var pub = '../client/app/';
 //Each image must be in a folder. It must have an img.json and img.JPG file.
@@ -45,9 +16,5 @@ for(var i = 0; i < dirs.length; i++) {
     var imgJSON = 
         JSON.parse((fs.readFileSync(pub + dirs[i] + "/img.json", 'ascii')));
     imgJSON.imgUrl = dirs[i] + "/img.JPG";
-    //imgJSON.imgUrl = Meteor.absoluteUrl() + "testTexture.jpg";
     StopList.insert(imgJSON);
-    //console.log("hi");
-    //create a new collection for each image, allowing for fine grained
-    //caching
 }     
