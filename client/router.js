3b24762424f12345a75a86fc070315d1321fc086
Thomas Truongchau <ttruongchau@gmail.com>
2014-04-09 10:55:22 -0400

    styled landing page (v1)

--- client/router.js
+++ client/router.js
@@ -8,6 +8,21 @@ Router.map(function () {
         template: 'home'
     });
 
+    this.route('about', {
+      path: '/about',
+      template: 'about'
+    });
+
+    this.route('contact', {
+      path: '/contact',
+      template: 'contact'
+    });
+
+    this.route('team', {
+      path: '/team',
+      template: 'team'
+    });
+
     this.route('tour', {
         path: '/tour',
         template: 'tour',
