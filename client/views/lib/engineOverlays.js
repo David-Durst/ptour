d3bbd07a32709352e5a677ebc943aa48acc664b7
David Durst <davidbdurst@gmail.com>
2014-04-17 00:06:46 -0400

    changed point of interest to the desired texture

--- client/views/lib/engineOverlays.js
+++ client/views/lib/engineOverlays.js
@@ -1,18 +1,24 @@
 Template.tour.engineOverlays = function (THREE, ret) {
-    ret.drawCircle = function(p) {
-        var circle = new THREE.Mesh(new THREE.CircleGeometry(3,32), new THREE.MeshNormalMaterial());  
-        circle.position.x = p.pX;
-        circle.position.y = p.pY;
-        circle.position.z = p.pZ;
-        circle.lookAt(ret.camera.position);
-        circle.clickable = true;
-        circle.point = p
-        ret.scene.add(circle);
+    ret.drawPOI = function(p) {
+        var material = new THREE.MeshBasicMaterial( {
+            map: THREE.ImageUtils.loadTexture('landing/icons/point_24x24.png'),
+            transparent:true,
+            opacity: 0.9,
+            color: 0x000000
+        });
+        var poi = new THREE.Mesh(new THREE.CircleGeometry(4,32), material);  
+        poi.position.x = p.pX;
+        poi.position.y = p.pY;
+        poi.position.z = p.pZ;
+        poi.lookAt(ret.camera.position);
+        poi.clickable = true;
+        poi.point = p
+        ret.scene.add(poi);
     }
 
     ret.drawPoints = function() {
         var curStop = Template.tour.data.StopList.findOne({id:Session.get('locId')});
-        $.each(curStop.points, function (i,p) { ret.drawCircle(p) });
+        $.each(curStop.points, function (i,p) { ret.drawPOI(p) });
     }
 
     ret.pointClicked = function (intersects) {
