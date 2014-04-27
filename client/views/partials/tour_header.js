96d8d7ee5aa0ad79e07ae8727601f2788fa34b68
Shwetha Raghuraman <sraghura@princeton.edu>
2014-04-27 15:30:03 -0400

    -Clicking away from search box hides dropdown. -Pressing enter does not submit searchbox form.

--- client/views/partials/tour_header.js
+++ client/views/partials/tour_header.js
@@ -21,16 +21,23 @@ Template.tour_header.setUpAutoComplete = function () {
 	    Template.tour.updateLeftOverlay();
 	}
     });
+
+    /* Anything that gets to the document
+       will hide the dropdown */
+    $(document).click(function(){
+	$(".ui-autocomplete").hide();
+    });
+
+    /* Clicks within the dropdown won't make
+       it past the dropdown itself */
+   $(".ui-autocomplete").click(function(e){
+	e.stopPropagation();
+    });
+
+    /* Do not submit form on enter */
+   $('#searchForm').submit(function(e) {
+	if (!$('#search').val()) {
+	    e.preventDefault();
+	}       
+    });
 };
-
-/* Anything that gets to the document
-   will hide the dropdown */
-$(document).click(function(){
-  $("#dropdown").hide();
-});
-
-/* Clicks within the dropdown won't make
-   it past the dropdown itself */
-$("#dropdown").click(function(e){
-  e.stopPropagation();
-});
