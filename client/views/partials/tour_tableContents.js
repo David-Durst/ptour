229645ce6a5896f27cb7c68a501238cd7256ee6e
Thomas Truongchau <ttruongchau@gmail.com>
2014-04-28 20:24:59 -0400

    included prev/next buttons on tablecontents

--- client/views/partials/tour_tableContents.js
+++ client/views/partials/tour_tableContents.js
@@ -11,33 +11,38 @@ Template.tour_tableContents.events({
     console.log("id: " + e.currentTarget.id);
     console.log(currPosx);
 
-    $('li.active').removeClass("active");
-    $("#" + e.currentTarget.id).addClass("active").animate("fade",50);
 
     if (diff < 0) { // move right
-        $(".graph-item").animate({
-          left: "+=" + offset + "px",
-        }, "slow" );
+      $(".graph-item").animate({
+        left: "+=" + offset + "px",
+      }, 800, "easeInOutExpo" );
     } else {          // move left
-        $(".graph-item").animate({
-            left: "-=" + offset + "px",
-        }, "slow" );
+      $(".graph-item").animate({
+          left: "-=" + offset + "px",
+      }, 800, "easeInOutExpo" );
+    }
+
+    $('li.active').removeClass("active");
+    $("#" + e.currentTarget.id).addClass("active").animate("fade",600);
+
+  },
+  /* Left/Right links on graph */
+  'click #control-left':function(e) {
+    var LTHRESHOLD = 22;
+    if ($("#scene01").offset().left < LTHRESHOLD) {
+      $(".graph-item").animate({
+        left: "+=300px",
+      }, 800, "easeInOutExpo" );
+    }
+  },
+  'click #control-right':function(e) {
+    var RTHRESHOLD = $(window).width() * 0.85;
+    if ($("#scene15").offset().left > RTHRESHOLD) {
+      $(".graph-item").animate({
+        left: "-=300px",
+      }, 800, "easeInOutExpo" );
     }
   }
-  //'click .active':function(e) {}
+
 });
 
-//   /* For L/R Links that we don't necessarily need */
-//   $("#right").click(function(e) {
-//     $("li").animate({
-//       left: "-=150px",
-//     }, "fast" );
-//   });
-
-//   $("#left").click(function(e) {
-//     $("ul").animate({
-//       left: "+=150px",
-//     }, "fast" );
-//   });
-
-// }
